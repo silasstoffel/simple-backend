deafc1a15e20940d914b6bd5976eea6a04862016
Silas Stoffel <silasstofel@gmail.com>
2020-03-26 23:35:18 -0300

    fixes sql commands

--- database.sql
+++ database.sql
@@ -1,62 +1,35 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: 25-Nov-2019 às 17:23
--- Versão do servidor: 5.7.14
--- PHP Version: 5.6.25
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `simple_backend`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `user`
---
-
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(90) COLLATE utf8_unicode_ci NOT NULL,
   `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `mobile_phone` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
-  `active` int(11) NOT NULL DEFAULT '1'
+  `active` int(11) NOT NULL DEFAULT '1',
+  `can_delete` smallint NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Indexes for dumped tables
---
+ALTER TABLE `user` ADD UNIQUE KEY `name_unique` (`email`);
 
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name_unique` (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Default insert:
+INSERT INTO `user` (
+  `name`,
+  `email`,
+  `password_hash`,
+  `mobile_phone`,
+  `created_at`,
+  `updated_at`,
+  `active`,
+  `can_delete`
+) VALUES (
+  'Mestre TagTec',
+  'mestre@tagtec.com.br',
+  '$2y$10$uhLbfnHJzz9mz5blzIZ3dOfTKIRewFXiO3YuNZ.4V6omuKFvTHo8i',
+  '27996354103',
+  '2020-03-26 22:19:52',
+  '2020-03-26 22:19:52',
+   1,
+   0
+);
